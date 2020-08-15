9446ec1e49d3cc477fa0eafaf6d59d943761337b
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-08-15 12:45:10 -0400

    mongoose deleted

--- server.js
+++ server.js
@@ -17,18 +17,7 @@ app.get("/api/config", (_, res) => {
 app.get('*', (_, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
-mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/react-portfolio", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Successfully connected to database.");
-  })
-  .catch((err) => {
-    console.log("Unable to connect to database.");
-    console.log(err);
-  });
+
 
 app.listen(PORT, () => {
   console.log(`Express server is running on http://localhost:${PORT}`);
